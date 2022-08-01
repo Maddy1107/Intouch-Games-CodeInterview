f613cde0ce2b7807860c389a4c4434fe968d9d4f
Nilankar Deb <69686900+Maddy1107@users.noreply.github.com>
2022-08-01 13:59:23 +0100

    commits

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ class ITGTechTask
     private canvas: HTMLCanvasElement;
     private context: CanvasRenderingContext2D;
 
-    private previousUpdateTime: number = 0;
+    //private previousUpdateTime: number = 0;
 
     private backgroundImage: HTMLImageElement = new Image();
 
@@ -12,15 +12,15 @@ class ITGTechTask
         this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
         this.context = this.canvas.getContext("2d");
 
-        this.backgroundImage.src = "graphics/background_safe_minigame.png";
+        this.backgroundImage.src = "src/graphics/background_safe_minigame.png";
         this.backgroundImage.onload = this.update.bind( this );
     }
 
     // Main Game Loop
     public update( currentTime: number = 0 ): void
     {
-        const deltaTime: number = currentTime - this.previousUpdateTime;
-        this.previousUpdateTime = currentTime;
+        //const deltaTime: number = currentTime - this.previousUpdateTime;
+        //this.previousUpdateTime = currentTime;
 
         // Clear the canvas
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -28,11 +28,6 @@ class ITGTechTask
         // Draw the background image
         this.context.drawImage( this.backgroundImage, 0, 0 );
 
-        // Draw some text - REMOVE THIS LATER!
-        this.context.font = "30px Arial";
-        this.context.fillText("ITG Technical Task",
-            Math.floor(Math.random() * (this.canvas.width)) + 1,
-            Math.floor(Math.random() * this.canvas.height) + 1);
 
         window.requestAnimationFrame( this.update.bind( this ) )
       }
